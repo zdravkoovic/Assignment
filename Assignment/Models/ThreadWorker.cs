16537b3d4d8c1d11fefaf6ce9bd2b47d6d91fbdd
zdravk00vic <zdravkoovic.nikola@gmail.com>
2026-05-22 18:42:55 +0200

    feat: add ThreadWorker model with progress calculation logic

---
+++ Assignment/Models/ThreadWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Assignment.Models
+{
+    public class ThreadWorker
+    {
+        private static readonly Random _random = new Random();
+
+        public int Duration { get; set; }
+        public int Elapsed { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public double Progress =>
+            Duration == 0 ? 0 : Math.Min(100.0 * Elapsed / Duration, 100);
+
+        public ThreadWorker()
+        {
+            Duration = _random.Next(10, 51);
+            IsActive = true;
+        }
+
+        public void Cancel()
+        {
+            IsActive = false;
+        }
+    }
+}
--- Assignment/ViewModels/LoadersViewModel.cs
+++ Assignment/ViewModels/LoadersViewModel.cs
@@ -9,6 +9,48 @@ namespace Assignment.ViewModels
 {
     public class LoadersViewModel : ViewModelBase
     {
+        private readonly Timer _timer;
+        public ObservableCollection<ThreadWorker> Workers { get; }
 
+        public double TotalProgress
+        {
+            get
+            {
+                var activeWorkers = Workers.Where(w => !w.IsActive).ToList();
+
+                if (!activeWorkers.Any())
+                    return 0;
+
+                return activeWorkers.Average(w => w.Progress);
+            }
+        }
+
+        public LoadersViewModel()
+        {
+            Workers = new ObservableCollection<ThreadWorker>
+            {
+                new ThreadWorker(),
+                new ThreadWorker(),
+                new ThreadWorker()
+            };
+
+            _timer = new Timer(1000);
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.AutoReset = true;
+            _timer.Start();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            foreach(var worker in Workers.Where(w => w.IsActive))
+            {
+                if(worker.Elapsed < worker.Duration)
+                {
+                    worker.Elapsed++;
+                }
+            }
+
+            OnPropertyChanged(nameof(TotalProgress));
+        }
     }
 }
