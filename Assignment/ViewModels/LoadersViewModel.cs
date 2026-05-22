e5cff323e4ba8fdf324472f9f8a67b9277944ff5
zdravk00vic <zdravkoovic.nikola@gmail.com>
2026-05-22 21:02:33 +0200

    feat: add LoadersViewModel with centralized timer and total progress logic

--- Assignment/ViewModels/LoadersViewModel.cs
+++ Assignment/ViewModels/LoadersViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Assignment.Commands;
+using Assignment.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Assignment.ViewModels
 {
