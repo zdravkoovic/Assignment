40f241576a56f7ff3d132fb06a6e8f5e9a385abf
zdravk00vic <zdravkoovic.nikola@gmail.com>
2026-05-23 23:05:54 +0200

    feat: add ToDo list display

---
+++ Assignment/Views/PriorityToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Assignment.Views
+{
+    public class PriorityToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((int)value)
+            {
+                case 1:
+                    return new SolidColorBrush(Color.FromArgb(40, 255, 0, 0));
+                case 2:
+                    return new SolidColorBrush(Color.FromArgb(40, 255, 165, 0));
+                case 3:
+                    return new SolidColorBrush(Color.FromArgb(40, 0, 128, 0));
+                default:
+                    return Brushes.Transparent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
